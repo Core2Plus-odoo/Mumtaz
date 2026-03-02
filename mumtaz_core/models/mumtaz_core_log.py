83b526e4cd6fcda91861b6d097e9619356a7875a
Core 2 Plus <49093704+Core2Plus-odoo@users.noreply.github.com>
2026-03-02 09:55:20 +0500

    fix(odoo19): finalize compatibility updates for 19.0-20260301 CE

---
+++ mumtaz_core/models/mumtaz_core_log.py
@@ -0,0 +1,42 @@
+from odoo import fields, models
+
+
+class MumtazCoreLog(models.Model):
+    _name = "mumtaz.core.log"
+    _description = "Mumtaz Core Action Log"
+    _order = "create_date desc"
+    _check_company_auto = True
+
+    company_id = fields.Many2one("res.company", required=True, index=True, check_company=True)
+    user_id = fields.Many2one("res.users", required=True, index=True, check_company=True)
+    module_name = fields.Char(required=True)
+    action = fields.Char(required=True)
+    request_payload = fields.Text()
+    response_payload = fields.Text()
+    level = fields.Selection(
+        [("info", "Info"), ("warning", "Warning"), ("error", "Error")],
+        default="info",
+        required=True,
+    )
+
+    def log_action(
+        self,
+        module_name,
+        action,
+        company,
+        user,
+        request_payload=None,
+        response_payload=None,
+        level="info",
+    ):
+        return self.create(
+            {
+                "company_id": company.id,
+                "user_id": user.id,
+                "module_name": module_name,
+                "action": action,
+                "request_payload": request_payload,
+                "response_payload": response_payload,
+                "level": level,
+            }
+        )
